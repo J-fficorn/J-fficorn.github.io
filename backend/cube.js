1efe97ed5cd65be616067ebe06a81414d2994e1d
Gefficorn <67201794+J-fficorn@users.noreply.github.com>
2022-01-10 17:15:47 -0500

    Update cube.js

--- backend/cube.js
+++ backend/cube.js
@@ -1,38 +1,24 @@
-var Color = {
-    W : "W", Y : "Y",
-    B : "B", G : "G",
-    R : "R", O : "O",
-};
-
-var Turn = {
-    F : "f", B : "b",
-    T : "t", U : "u",
-    R : "r", L : "l",
-    C : false, CC: true,
-};
-
-var changesC  = [6, 2, -2, 4];
-var changesCC = [2, 4, 6, -2];
+var Color = { W : "W", Y : "Y", B : "B", G : "G", R : "R", O : "O" };
+var Turn = { F : "f", B : "b", T : "t", U : "u", R : "r", L : "l", C : false, CC: true };
+var edges = [1, 3, 5, 7], corners = [0, 2, 6, 8]; //4 is center
+var changesC  = [6, 2, -2, 4], changesCC = [2, 4, 6, -2];
 var allFaces = ["f", "b", "t", "u", "r", "l"];
 
 function checkFace(face) {
-    var c = face[0];
+    var c = face[4];
     for (var i = 1; i < 9; i++) {
-        if (face[i] != c) {
-            return false
-        }
+        if (face[i] != c) { return false }
     }
     return true
 }
     
 function checkCube(cube) {
     for (var i = 0; i < 6; i++) {
-        if (!checkFace(cube.full[i])) {
-            return false
-        }
+        if (!checkFace(cube.full[i])) { return false }
     }
     return true
 }
+
 
 class Cube {
 
@@ -52,48 +38,35 @@ class Cube {
         switch (side) {
             case "f":
                 newFace = [...this.f]; refFace = this.f;
-                relT = [...this.t]; refT = this.t;
-                relU = [...this.u]; refU = this.u;
-                relR = [...this.r]; refR = this.r;
-                relL = [...this.l]; refL = this.l;
+                relT = [...this.t]; refT = this.t; relU = [...this.u]; refU = this.u;
+                relR = [...this.r]; refR = this.r; relL = [...this.l]; refL = this.l;
                 break;
             case "b":
                 newFace = [...this.b]; refFace = this.b;
-                relT = [...this.t]; refT = this.t;
-                relU = [...this.u]; refU = this.u;
-                relR = [...this.l]; refR = this.l;
-                relL = [...this.r]; refL = this.r;
+                relT = [...this.t]; refT = this.t; relU = [...this.u]; refU = this.u;
+                relR = [...this.l]; refR = this.l; relL = [...this.r]; refL = this.r;
                 break;
             case "t":
                 newFace = [...this.t]; refFace = this.t;
-                relT = [...this.b]; refT = this.b;
-                relU = [...this.f]; refU = this.f;
-                relR = [...this.r]; refR = this.r;
-                relL = [...this.l]; refL = this.l;
+                relT = [...this.b]; refT = this.b; relU = [...this.f]; refU = this.f;
+                relR = [...this.r]; refR = this.r; relL = [...this.l]; refL = this.l;
                 break;
             case "u":
                 newFace = [...this.u]; refFace = this.u;
-                relT = [...this.f]; refT = this.f;
-                relU = [...this.b]; refU = this.b;
-                relR = [...this.r]; refR = this.r;
-                relL = [...this.l]; refL = this.l;
+                relT = [...this.f]; refT = this.f; relU = [...this.b]; refU = this.b;
+                relR = [...this.r]; refR = this.r; relL = [...this.l]; refL = this.l;
                 break;
             case "r":
                 newFace = [...this.r]; refFace = this.r;
-                relT = [...this.t]; refT = this.t;
-                relU = [...this.u]; refU = this.u;
-                relR = [...this.b]; refR = this.b;
-                relL = [...this.f]; refL = this.f;
+                relT = [...this.t]; refT = this.t; relU = [...this.u]; refU = this.u;
+                relR = [...this.b]; refR = this.b; relL = [...this.f]; refL = this.f;
                 break;
             case "l":
                 newFace = [...this.l]; refFace = this.l;
-                relT = [...this.t]; refT = this.t;
-                relU = [...this.u]; refU = this.u;
-                relR = [...this.f]; refR = this.f;
-                relL = [...this.b]; refL = this.b;
+                relT = [...this.t]; refT = this.t; relU = [...this.u]; refU = this.u;
+                relR = [...this.f]; refR = this.f; relL = [...this.b]; refL = this.b;
                 break;
-            default:
-                console.log("Unknown Side");
+            default: console.log("Unknown Side");
         }
         for (var i = 0; i < 9; i++) {
             if (i == 4) { continue; }
@@ -116,119 +89,88 @@ class Cube {
             relU[0] = refL[2]; relU[1] = refL[5]; relU[2] = refL[8];
             relL[2] = refT[6]; relL[5] = refT[7]; relL[8] = refT[8];
         }
-        /* t6 = l8, t7 = l5, t8 = l2; +2, -2, -6
-         * r0 = t6, r3 = t7, r6 = t8; +6, +4, +2
-         * b0 = r0, b1 = r3, b2 = r6;  0, +2, +4
-         * l2 = b0, l5 = b1, l8 = b2; -2, -4, -6
-         */
         switch (side) {
-            case "f":
-                this.f = newFace;
-                this.t = relT; this.u = relU;
-                this.r = relR; this.l = relL;
-                break;
-            case "b":
-                this.b = newFace; break;
-                this.t = relT; this.u = relU;
-                this.l = relR; this.r = relL;
-            case "t":
-                this.t = newFace; break;
-                this.b = relT; this.f = relU;
-                this.r = relR; this.l = relL;
-            case "u":
-                this.u = newFace; break;
-                this.f = relT; this.b = relU;
-                this.r = relR; this.l = relL;
-            case "r":
-                this.r = newFace; break;
-                this.t = relT; this.u = relU;
-                this.b = relR; this.f = relL;
-            case "l":
-                this.l = newFace; break;
-                this.t = relT; this.u = relU;
-                this.f = relR; this.b = relL;
-            default:
-                console.log("Unknown Side");
+            case "f": this.f = newFace; this.t = relT; this.u = relU; this.r = relR; this.l = relL; break;
+            case "b": this.b = newFace; this.t = relT; this.u = relU; this.l = relR; this.r = relL; break;
+            case "t": this.t = newFace; this.b = relT; this.f = relU; this.r = relR; this.l = relL; break;
+            case "u": this.u = newFace; this.f = relT; this.b = relU; this.r = relR; this.l = relL; break;
+            case "r": this.r = newFace; this.t = relT; this.u = relU; this.b = relR; this.f = relL; break;
+            case "l": this.l = newFace; this.t = relT; this.u = relU; this.f = relR; this.b = relL; break;
+            default: console.log("Unknown Side");
         }
         this.full = [this.f, this.b, this.t, this.u, this.r, this.l];
+        this.clrs = findColorIndices(this.full);
         this.turns++;
         return
     }
 
-    findBase() {
-        var maxBase = 0;
-        var side = "";
-        for (var i = 0; i < 6; i++) {
-            var cur = 0;
-            var mid = this.full[i][4];
-            for (var j = 0; j < 9; j++) {
-                if (this.full[i][j] == mid) {
-                    cur++;
-                }
-            }
-            if (cur > maxBase) {
-                maxBase = cur;
-                side = allFaces[i];
-            }
+    solveBase() {
+        var rel;
+        while (!checkFace(full[0])) {
+            /* cases:
+             * sides 1 3 5, top 1 3 5 7 (implement against overcounting overcounted)
+             * so sides top/top side, sides side/connecting side side, sides bottom/connecting bottom side
+             * if 1, spin top until @ color of side
+             * catalog of moves:
+             * from side to top
+             * from top to aligned top
+             * from top to base
+             */
+            /* corners
+             * from bottom to top
+             * from top to bottom
+             * fiddle corners
+             */
         }
-        console.log(maxBase + side);
-        return side
     }
 
-    toString() {
+    toString(html) {
         var s = "";
         for (var i = 0; i < 6; i++) {
             switch (i) {
-                case 0: s += "f:\n"; break;
-                case 1: s += "b:\n"; break;
-                case 2: s += "t:\n"; break;
-                case 3: s += "u:\n"; break;
-                case 4: s += "r:\n"; break;
-                case 5: s += "l:\n"; break;
+                case 0: s += "f:"; break;
+                case 1: s += "b:"; break;
+                case 2: s += "t:"; break;
+                case 3: s += "u:"; break;
+                case 4: s += "r:"; break;
+                case 5: s += "l:"; break;
             }
+            s += (html ? "<br>" : "\n");
             for (var j = 0; j < 3; j++) {
-                s += (this.full[i][3 * j] + " " + this.full[i][3 * j + 1] + " " + this.full[i][3 * j + 2] + "\n");
-                //0 1 2, 3 4 5, 6 7 8??
+                s += (this.full[i][3 * j] + " " + this.full[i][3 * j + 1] + " " + this.full[i][3 * j + 2] + (html ? "<br>" : "\n"));
             }
-            s += "\n";
+            s += (html ? "<br>" : "\n");
         }
-        s += "IS RUBIX SOLVED? " + (checkCube(rubix) ? "YES" : "NO") + "\n";
-        s += "NUMBER OF TURNS: " + this.turns + "\n";
+        s += "IS RUBIX SOLVED? " + (this.solved ? "YES" : "NO") + (html ? "<br>" : "\n");
+        s += "NUMBER OF TURNS: " + this.turns + (html ? "<br>" : "\n");
         return s
     }
 }
 
-var fFace = [Color.W, Color.Y, Color.B, 
-             Color.G, Color.R, Color.O, 
-             Color.W, Color.Y, Color.B];
+var fFace = [Color.R, Color.G, Color.O, 
+             Color.B, Color.W, Color.B, 
+             Color.Y, Color.B, Color.O];
 
-var bFace = [Color.Y, Color.W, Color.G,
-             Color.B, Color.O, Color.W,
-             Color.Y, Color.R, Color.W];
+var bFace = [Color.O, Color.R, Color.O,
+             Color.W, Color.Y, Color.Y,
+             Color.R, Color.W, Color.G];
 
-var tFace = [Color.R, Color.G, Color.G,
-             Color.O, Color.G, Color.W,
-             Color.O, Color.R, Color.O];
+var tFace = [Color.W, Color.Y, Color.W,
+             Color.Y, Color.B, Color.R,
+             Color.G, Color.R, Color.G];
 
-var uFace = [Color.O, Color.B, Color.W,
-             Color.Y, Color.B, Color.O,
-             Color.G, Color.G, Color.R];
+var uFace = [Color.R, Color.O, Color.B,
+             Color.R, Color.G, Color.O,
+             Color.R, Color.G, Color.W];
 
-var rFace = [Color.Y, Color.R, Color.O, 
-             Color.W, Color.W, Color.R, 
-             Color.R, Color.Y, Color.B];
+var rFace = [Color.Y, Color.W, Color.B, 
+             Color.W, Color.R, Color.O, 
+             Color.Y, Color.G, Color.B];
 
-var lFace = [Color.Y, Color.B, Color.G,
-             Color.B, Color.Y, Color.O,
-             Color.R, Color.G, Color.B];
+var lFace = [Color.G, Color.O, Color.Y,
+             Color.G, Color.O, Color.Y,
+             Color.W, Color.B, Color.B];
 
 var listRepresentation = [fFace, bFace, tFace, uFace, rFace, lFace];
 let rubix = new Cube(listRepresentation);
-console.log(rubix.toString());
-rubix.turnFace(Turn.F, Turn.CC);
-console.log(rubix.toString());
-rubix.turnFace(Turn.F, Turn.C);
-console.log(rubix.toString());
-rubix.turnFace(Turn.T, Turn.C);
-console.log(rubix.toString());
-console.log(rubix.findBase());
+console.log(rubix.toString(false));
